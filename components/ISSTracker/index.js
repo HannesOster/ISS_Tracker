18191e002196a2f56276c8c05fe8e378bc16c0be
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-05 11:11:22 +0200

    add useSWR

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
@@ -9,17 +10,10 @@ export default function ISSTracker() {
     longitude: 0,
     latitude: 0,
   });
+  const { data, error, isLoading } = useSWR(URL);
 
   async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
+    setCoords({ longitude: data.longitude, latitude: data.latitude });
   }
 
   useEffect(() => {
@@ -31,6 +25,12 @@ export default function ISSTracker() {
       clearInterval(timer);
     };
   }, []);
+  if (isLoading) {
+    return <h1>Loading ...</h1>;
+  }
+  if (error) {
+    return <h1>{error.message}</h1>;
+  }
 
   return (
     <main>
